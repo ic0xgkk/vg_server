d4b2144a7c79c49e07270fb58987bda366858c5d
ic0xgkk <ic0xgkk@gmail.com>
2018-04-05 00:25:19 +0800

    基本完成的迷你版

--- server.c
+++ server.c
@@ -1,3 +1,12 @@
+/* 作者：雪糕 <ic0xgkk@gmail.com>
+   首页：www.xuegaogg.com
+   关于本程序： 本程序主要用于DJI的RoboMaster的机器人调参使用，传统的蓝牙调参和无线串口调参问题参差不齐，
+            无线串口丢包严重，并且无法存储数据，蓝牙调参范围太小，稳定性也不高，故可以使用本程序搭配一个E
+            SP8266和STM32和android终端来进行无线调参，还算是方便点吧。
+              本程序为服务端，推荐自主编译openwrt或者lede，将其rootfs转移到SD卡分区，然后安装gcc和mak
+            e等相关依赖，将服务端启动在嵌入式linux设备上边将会是一个划算又省事的方案。
+*/
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -29,7 +38,13 @@
 
 //全局定义区域
 #define BUFFER_SIZE 1024   //TCP接受的缓冲区大小
-char listen_addr[] = "192.168.50.66";   //监听IP地址
+#define LISTEN_PORT 6666
+//char listen_addr[] = "192.168.50.66";   //监听IP地址
+
+static struct device{
+    char ip[16];
+    int cid;
+}device_data[32];
 
 
 //自定义函数区域
@@ -37,13 +52,14 @@ void server_func(void *args);
 int files_check(void);
 int data_manipulation(char buffer[BUFFER_SIZE]);
 size_t file_size(char* filename);
-
-
+int read_device(void);
+int before_accept(struct sockaddr_in stSockAddr);
 
 int main(void)
 {
   printf("\n---------------------------------------------------------");
   printf("\n                 Vehicle God Data Center                 ");
+  printf("\n                       车神数据中心                        ");
   printf("\n*********************************************************\n");
 
   if ( files_check() == -1 )
@@ -51,46 +67,34 @@ int main(void)
     exit(EXIT_FAILURE);
   }
 
-  static struct sockaddr_in stSockAddr;
+  if( read_device() != 1 )
+  {
+    printf("ERROR: Read device failed.\n");
+    exit(EXIT_FAILURE);
+  }
+
   time_t tNow = 0;
   int ConnectFD = 0;
-  pthread_t thread;       //创建不同的子线程以区别不同的客户端
-  int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-  if(-1 == SocketFD)
-  {
-    perror("ERROR:Can not create socket ");
-    close(SocketFD);
-    exit(EXIT_FAILURE);
-  }
-
+  struct sockaddr_in stSockAddr;
   memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
   stSockAddr.sin_family = AF_INET;
-  stSockAddr.sin_port = htons(6666);  //Listen port : 6666/tcp
+  stSockAddr.sin_port = htons(LISTEN_PORT);
   stSockAddr.sin_addr.s_addr = INADDR_ANY;
 
-  if( bind(SocketFD,(struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)) == -1)
-  {
-    perror("ERROR:Error bind failed ");
-    close(SocketFD);
-    exit(EXIT_FAILURE);
-  }
-  else printf("- Bind Success.\n");
-
-
-  if( listen(SocketFD, 128) == -1 )
-  {
-    perror("ERROR:Error listen failed.\n");
-    close(SocketFD);
-    exit(EXIT_FAILURE);
-  }
-  else printf("- Listen Success.\n");
+  pthread_t thread;       //创建不同的子线程以区别不同的客户端
 
   char buffer[256];
   memset(buffer, 0, sizeof(buffer));
-  static struct sockaddr_in client_addr;
+
+  int SocketFD= before_accept(stSockAddr);
+
+  struct sockaddr_in client_addr;
+
   socklen_t addr_lenth=16;
+
   char host[NI_MAXHOST],service_port[NI_MAXSERV];
+
   while(1)
   {
     time(&tNow);
@@ -107,7 +111,8 @@ int main(void)
     if(ConnectFD > 0)
     {
             //由于同一个进程内的所有线程共享内存和变量，因此在传递参数时需作特殊处理值传递
-            if (getnameinfo( (const struct sockaddr *)&client_addr,16, host, NI_MAXHOST,service_port,NI_MAXSERV, NI_NUMERICSERV) == 0) printf("Thread Created: Accept client from %s:%s \n", host, service_port);
+            if (getnameinfo( (const struct sockaddr *)&client_addr,16, host, NI_MAXHOST,service_port,NI_MAXSERV, NI_NUMERICSERV) == 0)
+                printf("Thread Created: Accept client from %s:%s \n", host, service_port);
             void *args=(void *)malloc( sizeof(int)+sizeof(client_addr)+sizeof(socklen_t) );
             memcpy( &args,&ConnectFD,sizeof(int) );
             memcpy( &args+sizeof(int),&client_addr,sizeof(client_addr) );
@@ -125,7 +130,7 @@ int main(void)
 void server_func(void *args)
 {
     char buffer[BUFFER_SIZE];
-    char host_pt[NI_MAXHOST],service_port_pt[NI_MAXSERV];
+//    char host_pt[NI_MAXHOST],service_port_pt[NI_MAXSERV];
     int connfd;
     socklen_t addr_lenth_pt;
     struct sockaddr_in cli_addr;
@@ -133,23 +138,25 @@ void server_func(void *args)
     memcpy(&cli_addr,args+sizeof(int),sizeof(struct sockaddr) );
     memcpy(&addr_lenth_pt,args+sizeof(int)+sizeof(struct sockaddr) ,sizeof(socklen_t) );
 
-    getnameinfo( (struct sockaddr *)&cli_addr,16, host_pt, NI_MAXHOST,service_port_pt,NI_MAXSERV, NI_NUMERICSERV);
+    //int ss = getnameinfo( (struct sockaddr *)&cli_addr, 64, host_pt, NI_MAXHOST,service_port_pt,NI_MAXSERV, NI_NUMERICSERV);
         //printf("Reviced %lu bytes from %s:%s \n", (long)recvfrom(connfd, buffer, BUFFER_SIZE, 0,(struct sockaddr *) &cli_addr, &addr_lenth_pt) , host_pt, service_port_pt );
-    if( data_manipulation(buffer) != 0 )
-    {
-        perror("ERROR: Data manipulat failed ");
-    }
-
 
     while(1)
     {
-        while( recv(connfd, buffer, sizeof(buffer),0) > 0)
+        if( recv(connfd, buffer, sizeof(buffer),0) > 0)
         {
             printf("Buffer data: %s\n", buffer);
-            if( data_manipulation(buffer) != 0 )
+            if( data_manipulation(buffer) != 1 )
             {
                 perror("ERROR: Data manipulat failed ");
             }
+        }
+        else
+        {
+            printf("Thread Closed.\n");
+            shutdown(connfd, SHUT_RDWR);
+            close(connfd);
+            break;
         }
     }
 }
@@ -199,6 +206,68 @@ int files_check(void)
     return 1;
 }
 
+int read_device(void)
+{
+    FILE *fp=fopen("device","r");
+    int i=0;
+    while(!feof(fp))
+    {
+        fscanf(fp,"%s%d",device_data[i].ip, &device_data[i].cid );
+        i++;
+    }
+    fclose(fp);
+    return 1;
+
+}
+
+//报文第0位为ORI(方向位)，第2位为CID(设备标识符)，第1第3为分隔用的逗号，从第4位开始为数据位
+int data_manipulation( char buffer[BUFFER_SIZE] )
+{
+    if( buffer[0] == '1' )
+    {
+        int i=0;
+        while(1)
+        {
+            if( device_data[i].cid == buffer[2] )
+            {
+                break;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        struct sockaddr_in forward_addr;
+        if( inet_aton(device_data[i].ip,&forward_addr.sin_addr) == 0 )
+        {
+            return -1;
+        }
+
+        int forward_tcp_status=0;
+
+        if( forward_tcp_status == 0 )
+        {
+
+
+        }
+
+
+
+
+    }
+
+    if( buffer[0] == '2' )
+    {
+        printf("What the fuck????\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return 1;
+}
+
+
+/*
 //对数据进行分离存储，以及对data文件进行内存映射
 int data_manipulation( char buffer[BUFFER_SIZE] )
 {
@@ -230,6 +299,41 @@ int data_manipulation( char buffer[BUFFER_SIZE] )
 
 
     return 0;
+}
+
+*/
+
+
+int before_accept(struct sockaddr_in stSockAddr)
+{
+  int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+  if(-1 == SocketFD)
+  {
+    perror("ERROR:Can not create socket ");
+    close(SocketFD);
+    exit(EXIT_FAILURE);
+  }
+
+  if( bind(SocketFD,(struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)) == -1 )
+  {
+    perror("ERROR:Error bind failed ");
+    close(SocketFD);
+    exit(EXIT_FAILURE);
+  }
+  else printf("- Bind Success.\n");
+
+
+  if( listen(SocketFD, 128) == -1 )
+  {
+    perror("ERROR:Error listen failed.\n");
+    close(SocketFD);
+    exit(EXIT_FAILURE);
+  }
+
+  else printf("- Listen Success.\n");
+
+  return SocketFD;
 }
 
 size_t file_size(char filename[])
