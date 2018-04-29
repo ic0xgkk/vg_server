4fe061dc0cf0c71605faee0415540eea374ee6c6
ic0xgkk <ic0xgkk@gmail.com>
2018-04-29 21:15:38 +0800

    增加了FIFO

--- server.c
+++ server.c
@@ -6,40 +6,7 @@
               本程序为服务端，推荐自主编译openwrt或者lede，将其rootfs转移到SD卡分区，然后安装gcc和mak
             e等相关依赖，将服务端启动在嵌入式linux设备上边将会是一个划算又省事的方案。
 */
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <netdb.h>
-
-#include <sys/mman.h>  //内存文件映射
-
-#include <pthread.h>   //多线程
-
-
-#include <time.h>   //系统API读取UTC时间
-
-
-#include <sys/time.h>
-#include <malloc.h>
-
-
-#include <sys/stat.h>    //文件大小判断
-
-
-#include <fcntl.h>   //文件检查需要用到
-
-//全局定义区域
-#define BUFFER_SIZE 1024   //TCP接受的缓冲区大小
-#define LISTEN_PORT 6666
-#define DEVICE_AMOUNT 32
+#include "include.h"
 
 static struct device{
     char ip[16];
